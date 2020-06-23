bfe041f3b71e3ad16b18ef5dca3e131f4e07eb3f
traore <tresonance2@gmail.com>
2020-06-23 03:53:47 +0200

    emberjs-tuto8

---
+++ emberjs_tuto4/app/reopenclass.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+export default function () {
+    // reopen() method for instances
+    var Person = Ember.Object.extend({
+        firstName: null,
+        lastName: null,
+    });
+    // adding new variable to the Person class
+    Person.reopen({
+        middleName: 'Smith',
+    });
+    document.write('Middle Name: ' + Person.create().get('middleName'));
+    document.write("<br>");
+    // reopenClass() method for classes
+    Person.reopenClass({
+        //creating new function for class Person
+        openClass: function () {
+            return Person.create({ isMan: true });
+        }
+    });
+    document.write('isMan: ' + Person.openClass().get('isMan'));
+}
